e53bc0f49cea6212ce5ccf405a7fb6dd3a475c52
Oliver Hine <git@oliy.co.uk>
2016-03-07 23:09:35 +0000

    Shadowing pedestal interceptors as per #2 (needs tests and/or examples)

---
+++ src/pedestal_api/helpers.clj
@@ -0,0 +1,33 @@
+(ns pedestal-api.helpers
+  (:require [io.pedestal.interceptor.helpers]
+            [pedestal-api.swagger :as swagger]))
+
+(defmacro defhelper [helper-name]
+  `(defmacro ~(symbol (str "def" helper-name))
+     [name# swagger# & args#]
+     `(def ~name#
+        (swagger/annotate ~swagger#
+                          (@~(ns-resolve 'io.pedestal.interceptor.helpers '~helper-name)
+                           (keyword ~name#)
+                           (fn ~@args#))))))
+
+;; shadows helper macros in io.pedestal.interceptor.helpers
+;; adding swagger metadata as the second argument
+;; e.g. (defhandler create-pet
+;;                  {:summary "Creates a pet"}
+;;                  [request]
+;;                  {:status 200
+;;                   :body "Created pet"})
+
+;; Note that pedestal recommends building interceptors directly, e.g.
+;; (i/interceptor {:name ::my-interceptor
+;;                 :before (fn [context] ... )
+
+
+(defhelper before)
+(defhelper after)
+(defhelper around)
+(defhelper on-request)
+(defhelper on-response)
+(defhelper handler)
+(defhelper middleware)
