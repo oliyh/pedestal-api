f17ed93005843e558bc9e6c3ba01a1a3bff9e06f
Oliver Hine <git@oliy.co.uk>
2016-03-08 19:45:27 +0000

    Potemkin isn't required at runtime either

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "A batteries-included API for Pedestal using Swagger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[frankiesardo/route-swagger "0.1.0"]
-                 [potemkin "0.4.3"]]
+  :dependencies [[frankiesardo/route-swagger "0.1.0"]]
   :repl-options {:init-ns user}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
                                        [io.pedestal/pedestal.service "0.4.1"]]}
              :dev {:source-paths ["dev" "example"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.5"]
+                   :dependencies [[potemkin "0.4.3"]
+                                  [org.clojure/tools.namespace "0.2.5"]
                                   [io.pedestal/pedestal.jetty "0.4.1"]
                                   [clj-http "2.0.1"]
 
