0292011037efc5f41f11bade1cc4915c973d8642
Oliver Hine <git@oliy.co.uk>
2016-06-06 21:58:45 +0100

    Taking advantage of pedestal's new content negotiation

--- src/pedestal_api/content_negotiation.clj
+++ src/pedestal_api/content_negotiation.clj
@@ -1,8 +1,9 @@
 (ns pedestal-api.content-negotiation
   (:require [clojure.string :as string]
             [io.pedestal.http :as service]
-            [io.pedestal.interceptor.chain :refer [terminate]]
+            [io.pedestal.interceptor.chain :refer [terminate enqueue*]]
             [io.pedestal.interceptor.helpers :as interceptor]
+            [io.pedestal.http.content-negotiation :as pcn]
             [linked.core :as linked]
             [route-swagger.doc :as sw.doc]
             [ring.util.response :as ring-response]))
@@ -11,13 +12,13 @@
   (interceptor/on-response
    ::edn-body
    (fn [response]
-      (let [body (:body response)
-            content-type (get-in response [:headers "Content-Type"])]
-        (if (and (coll? body) (not content-type))
-          (-> response
-              (ring-response/content-type "application/edn;charset=UTF-8")
-              (assoc :body (:body (service/edn-response body))))
-          response)))))
+     (let [body (:body response)
+           content-type (get-in response [:headers "Content-Type"])]
+       (if (and (coll? body) (not content-type))
+         (-> response
+             (ring-response/content-type "application/edn;charset=UTF-8")
+             (assoc :body (:body (service/edn-response body))))
+         response)))))
 
 (def default-serialisation-interceptors
   (linked/map
@@ -28,17 +29,16 @@
    "application/transit" service/transit-body))
 
 (defn- find-interceptor [serialisation-interceptors accept]
-  (when-not (string/blank? accept)
-    (first
-     (for [acceptable-type (-> (string/split accept #";")
-                               first
-                               (string/split #","))
-           [type i] serialisation-interceptors
-           :when (= type acceptable-type)]
-       i))))
+  (get serialisation-interceptors accept))
+
+(defn default-to [content-type]
+  (fn [ctx]
+    (println "defaulting!")
+    (assoc-in ctx [:request :accept] content-type)))
 
 (defn negotiate-response
-  ([] (negotiate-response default-serialisation-interceptors service/json-body))
+  ([] (negotiate-response default-serialisation-interceptors))
+  ([serialisation-interceptors] (negotiate-response serialisation-interceptors service/json-body))
   ([serialisation-interceptors default-serialiser]
    (sw.doc/annotate
     {:produces (keys serialisation-interceptors)
@@ -47,9 +47,17 @@
     (interceptor/around
      ::serialise-response
 
-     identity
-     ;; turned off until can work out what to do with things like text/plain,text/html,image/jpg etc
-     #_(fn [{:keys [request] :as ctx}]
+     (fn [ctx]
+       (enqueue* ctx (pcn/negotiate-content (keys serialisation-interceptors) {:no-match-fn identity})))
+
+     (fn [{:keys [request] :as ctx}]
+       (if-let [i (or (find-interceptor serialisation-interceptors (get-in request [:accept :field]))
+                      default-serialiser)]
+         (update ctx :io.pedestal.interceptor.chain/stack conj i)
+         ctx))))))
+
+;; turned off until can work out what to do with things like text/plain,text/html,image/jpg etc
+#_(fn [{:keys [request] :as ctx}]
          (let [accept (get-in request [:headers "accept"])]
            (if-not (find-interceptor serialisation-interceptors accept)
              (-> ctx
@@ -59,9 +67,3 @@
                                    :body (format "No serialiser could be found to generate '%s'."
                                                  accept)}))
              ctx)))
-
-     (fn [{:keys [request] :as ctx}]
-       (if-let [i (or (find-interceptor serialisation-interceptors (get-in request [:headers "accept"]))
-                      default-serialiser)]
-         (update ctx :io.pedestal.interceptor.chain/stack conj i)
-         ctx))))))
