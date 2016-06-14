d377e25a528031a37b37da4f2bea1156d026733d
Oliver Hine <git@oliy.co.uk>
2016-06-14 19:41:22 +0100

    Bumping route-swagger, fixes #13

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[frankiesardo/route-swagger "0.1.0"]]
+  :dependencies [[frankiesardo/route-swagger "0.1.1"]]
   :repl-options {:init-ns user}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
                                        [io.pedestal/pedestal.service "0.5.0"]]}
