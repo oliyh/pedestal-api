3f94bc125cd4e4ec233bf28b966e3aaabe502bfa
Oliver Hine <git@oliy.co.uk>
2016-09-24 20:09:25 +0100

    upgrading deps

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[frankiesardo/route-swagger "0.1.1"]]
+  :dependencies [[frankiesardo/route-swagger "0.1.2"]]
   :repl-options {:init-ns user}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
                                        [io.pedestal/pedestal.service "0.5.1"]]}
              :dev {:source-paths ["dev" "example"]
                    :dependencies [[potemkin "0.4.3"]
-                                  [org.clojure/tools.namespace "0.2.5"]
+                                  [org.clojure/tools.namespace "0.2.10"]
                                   [io.pedestal/pedestal.jetty "0.5.1"]
                                   [clj-http "2.0.1"]
 
-                                  [ch.qos.logback/logback-classic "1.1.2"
+                                  [ch.qos.logback/logback-classic "1.1.7"
                                    :exclusions [org.slf4j/slf4j-api]]
-                                  [org.slf4j/jcl-over-slf4j "1.7.7"]
-                                  [org.slf4j/jul-to-slf4j "1.7.7"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.7"]]}}
+                                  [org.slf4j/jcl-over-slf4j "1.7.21"]
+                                  [org.slf4j/jul-to-slf4j "1.7.21"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.21"]]}}
 
   ;; for heroku deployment of example
   :min-lein-version "2.0.0"
