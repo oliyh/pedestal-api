f120f9addc2683cb7477f0ddc33d84b2b9a54bf9
Oliver Hine <git@oliy.co.uk>
2017-01-20 18:50:18 +0000

    Updating example to latest pedestal-api

--- example/project.clj
+++ example/project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [pedestal-api "0.3.1-SNAPSHOT" :exclusions [prismatic/schema]]
-                 [prismatic/schema "1.1.1"]
-                 [io.pedestal/pedestal.service "0.5.1"]
-                 [io.pedestal/pedestal.jetty "0.5.1"]
+                 [pedestal-api "0.3.1" :exclusions [prismatic/schema]]
+                 [prismatic/schema "1.1.3"]
+                 [io.pedestal/pedestal.service "0.5.2"]
+                 [io.pedestal/pedestal.jetty "0.5.2"]
 
-                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.21"]
-                 [org.slf4j/jcl-over-slf4j "1.7.21"]
-                 [org.slf4j/log4j-over-slf4j "1.7.21"]
+                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.22"]
+                 [org.slf4j/jcl-over-slf4j "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j "1.7.22"]
                  [org.clojure/tools.logging "0.3.1"]]
   :main ^:skip-aot pedestal_api_example.server
   :target-path "target/%s"
