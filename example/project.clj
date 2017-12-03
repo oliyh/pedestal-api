1499397d2e745f1b46930c6939ac8e959764ab29
Oliver Hine <git@oliy.co.uk>
2017-12-03 14:35:47 +0000

    Version 0.3.2

--- example/project.clj
+++ example/project.clj
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [pedestal-api "0.3.1" :exclusions [prismatic/schema]]
                  [prismatic/schema "1.1.3"]
-                 [io.pedestal/pedestal.service "0.5.2"]
-                 [io.pedestal/pedestal.jetty "0.5.2"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [io.pedestal/pedestal.jetty "0.5.3"]
 
                  [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.22"]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-api "0.3.2-SNAPSHOT"
+(defproject pedestal-api "0.3.2"
   :description "A batteries-included API for Pedestal using Swagger"
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
