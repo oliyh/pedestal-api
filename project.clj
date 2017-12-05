810d9e82f3c921f7263656af95ca83d0ddf4663e
Oliver Hine <git@oliy.co.uk>
2017-12-05 09:53:39 +0000

    Version 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-api "0.3.3-SNAPSHOT"
+(defproject pedestal-api "0.3.4"
   :description "A batteries-included API for Pedestal using Swagger"
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
